d1d8d95abfe06c7811c784c349177637211e07d8
Filip Úradník <filip.uradnik9@gmail.com>
2022-06-30 20:01:47 +0200

    Add refreshing of seed

--- wppgen/decider.py
+++ wppgen/decider.py
@@ -1,7 +1,9 @@
 import random
 import datetime
 
-random.seed(datetime.date.today().isoformat())
+def refresh_seed():
+    random.seed(datetime.date.today().isoformat())
 
 def get_index(number_of_items: int):
+    refresh_seed()
     return random.randrange(number_of_items)
